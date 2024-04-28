80305594713bd6118515fe3cb48d768eb003bf1e
Adyatma <awan2403@gmail.com>
2024-04-28 15:31:57 +0700

    edit dataloader

--- load.py
+++ load.py
@@ -20,18 +20,16 @@ class CustomDataset(Dataset):
         image = cv2.imread(img_name)
         # OpenCV reads in BGR format, convert it to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        label = self.df.iloc[idx, 1]
 
-        # Convert NumPy array to PIL image
+        # Convert NumPy array to PIL image and apply transformations
         image = Image.fromarray(image)
-
         if self.transform:
             image = self.transform(image)
 
-        # Convert label to tensor
-        label = torch.tensor(label, dtype=torch.long)
+        label = self.df.iloc[idx, 1]
 
-        return {'image': image, 'label': label}
+        return image, label
+
 
 class ApplyTransform(object):
     def __init__(self, transform):
@@ -40,20 +38,3 @@ class ApplyTransform(object):
     def __call__(self, sample):
         image, label = sample['image'], sample['label']
         return {'image': self.transform(image), 'label': label}
-
-# # Example of usage
-# # Assuming you have defined your transformations if any (e.g., normalization, resizing, etc.)
-# # transform = transforms.Compose([transforms.ToTensor()])
-
-# # Initialize your custom dataset
-# dataset = CustomDataset(csv_file='/path/to/your/csv/file.csv',
-#                         image_dir='/path/to/your/image/directory/',
-#                         transform=None)  # Pass your transformations if any
-
-# # Initialize DataLoader
-# data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-# # Now you can iterate over the data_loader to get batches of images and labels
-# for batch in data_loader:
-#     images, labels = batch['image'], batch['label']
-#     # Your training/validation loop or any other processing here
